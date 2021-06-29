7adfec74027fc93ac17c82df8a80414bc64599af
SeungYongShim <syshim@me.com>
2021-06-30 00:15:40 +0900

    추가 테스트

--- src/WcfServiceLibrary1/IService1.cs
+++ src/WcfServiceLibrary1/IService1.cs
@@ -1,29 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.ServiceModel;
-using System.ServiceModel.Web;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace WcfServiceLibrary1
 {
+    using System.IO;
+    using System.ServiceModel;
+    using System.ServiceModel.Channels;
+    using System.ServiceModel.Web;
+
     // 참고: "리팩터링" 메뉴에서 "이름 바꾸기" 명령을 사용하여 코드 및 config 파일에서 인터페이스 이름 "IService1"을 변경할 수 있습니다.
     [ServiceContract]
     public interface IService1
     {
-        //[OperationContract(AsyncPattern = true)]
-        //[WebInvoke(Method = "Post", BodyStyle = WebMessageBodyStyle.Wrapped, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "getdataasync") ]
-        //Task<string> GetDataAsync(int value);
-
         [OperationContract]
-        [WebInvoke(Method = "Post", BodyStyle = WebMessageBodyStyle.Wrapped, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "getdata")]
-        string GetData(int value);
+        [WebInvoke(Method = "POST", BodyStyle = WebMessageBodyStyle.WrappedRequest, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "getdata1")]
+        Stream GetData1(int value);
 
         [OperationContract]
         [WebInvoke]
         CompositeType GetDataUsingDataContract(CompositeType composite);
-
-        // TODO: 여기에 서비스 작업을 추가합니다.
     }
 }
--- src/WcfServiceLibrary1/Service1.cs
+++ src/WcfServiceLibrary1/Service1.cs
@@ -1,16 +1,16 @@
-using System;
-using System.Threading.Tasks;
-
 namespace WcfServiceLibrary1
 {
-    // 참고: "리팩터링" 메뉴에서 "이름 바꾸기" 명령을 사용하여 코드 및 config 파일에서 클래스 이름 "Service1"을 변경할 수 있습니다.
+    using System;
+    using System.IO;
+    using System.ServiceModel.Web;
+    using System.Text;
+
     public class Service1 : IService1
     {
-        public string GetData(int value) => string.Format("You entered: {0}", value);
-
-        public async Task<string> GetDataAsync(int value)
+        public Stream GetData1(int value)
         {
-            return string.Format("You entered: {0}", value);
+            WebOperationContext.Current.OutgoingResponse.ContentType = "application/json; charset=utf-8";
+            return new MemoryStream(Encoding.UTF8.GetBytes($"You entered: {value}"));
         }
 
         public CompositeType GetDataUsingDataContract(CompositeType composite)
